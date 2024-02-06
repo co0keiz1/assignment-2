cb42bd56705ee7df53e874c5206631267c007778
Tore Nylén <toreny@kth.se>
2024-02-06 11:39:50 +0100

    feat: #3 download repo on CI action

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -2,11 +2,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+
+class GitDownload {
+    public static void main(String[] args) throws InvalidRemoteException, TransportException, GitAPIException {
+        Git.cloneRepository()
+                .setURI("https://github.com/group19-se24/assignment-2.git")
+                .setDirectory(new File("temp/repository"))
+                .call();
+    }
+
+
+    static void gitClone() throws InvalidRemoteException, TransportException, GitAPIException {
+        Git.cloneRepository()
+                .setURI("https://github.com/group19-se24/assignment-2.git")
+                .setDirectory(new File("temp/repository"))
+                .call();
+        System.out.println("REPO DOWNLOADED");
+    }
+}
 
 /**
  Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -29,6 +54,11 @@ public class ContinuousIntegrationServer extends AbstractHandler
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
+        try {
+            GitDownload.gitClone();
+        } catch (GitAPIException e) {
+            throw new RuntimeException(e);
+        }
         // 2nd compile the code
 
         response.getWriter().println("CI job done");
