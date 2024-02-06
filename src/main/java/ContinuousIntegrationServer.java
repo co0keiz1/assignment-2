7920a373a29970d653b72f69003b26648c5ea99d
Tore Nylén <toreny@kth.se>
2024-02-06 11:39:50 +0100

    feat: #3 clone via command line

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -2,11 +2,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+
+
+class GitDownload {
+
+
+    static void gitClone() {
+
+        new File("temp/repository");
+        System.out.println("REPO DOWNLOADED");
+    }
+}
+
+
 
 /**
  Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -14,13 +30,24 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
  */
 public class ContinuousIntegrationServer extends AbstractHandler
 {
+    // https://www.baeldung.com/java-delete-directory
+    boolean deleteDirectory(File directoryToBeDeleted) {
+        File[] allContents = directoryToBeDeleted.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        return directoryToBeDeleted.delete();
+    }
+
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
                        HttpServletResponse response)
             throws IOException, ServletException
     {
-        response.setContentType("text/html;charset=utf-8");
+        response.setContentType("text/html;charset=utf-16");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
 
@@ -29,6 +56,52 @@ public class ContinuousIntegrationServer extends AbstractHandler
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
+        System.out.println(request.toString());
+        System.out.println("HERE:");
+
+        StringBuilder payload = new StringBuilder();
+        String line;
+
+        try (BufferedReader reader = request.getReader()) {
+            while ((line = reader.readLine()) != null) {
+                payload.append(line).append('\n');
+            }
+        }
+
+        // Here, you can convert the payload to a JSON object using your preferred JSON library (e.g., Gson, Jackson)
+        // Example:
+        // JsonObject jsonObject = JsonParser.parseString(payload.toString()).getAsJsonObject();
+        // System.out.println(jsonObject.toString());
+
+        System.out.println("Payload: " + payload.toString());
+
+
+        deleteDirectory(new File("temp/repository"));
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command(
+                new String[]{"git", "clone", "https://github.com/group19-se24/assignment-2.git", "temp/repository"});
+        Process process = processBuilder.start();
+        try {
+            process.waitFor();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println();
         // 2nd compile the code
 
         response.getWriter().println("CI job done");
